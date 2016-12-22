5cb4d15d801379c3e0d156d6524c9b3babb62e5f
shengdewu <786222104@qq.com>
2016-12-22 16:54:17 +0800

    add testpool

---
+++ http_server/sample/TestThread.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "Thread.h"
+#include "Condition.h"
+
+class MyRunable : public Runable
+{
+public:
+	MyRunable():_cnt(0),_run(false){}
+	~MyRunable(){}
+
+	void run()
+	{
+		for(int i=0; i< 20; i ++)
+		{
+			_cnt += i;
+		}
+		_run = true;
+		_con.wait();
+	}
+
+	long getCnt()
+	{
+		return _cnt;
+	}
+
+	void notify()
+	{
+		_con.notify();
+	}
+
+	bool isRun()
+	{
+		return _run;
+	}
+
+private:
+	Condition	_con;
+	int			_cnt;
+	bool		_run;
+};
+
+int main(int argc, char *argv[])
+{
+	Thread thread;
+	MyRunable	r;
+	thread.start(r);
+	while(!r.isRun());
+	r.notify();
+	thread.join();
+	while(!thread.isRunning());
+	std::cout << "MyRunable's cnt = " << r.getCnt() << std::endl;
+	return 0;
+}
